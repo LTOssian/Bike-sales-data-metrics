a1744bfb12fd6a7eba72771d42949f3a446aab56
Louisan Tchitoula <t.louisan@outlook.fr>
2024-08-04 13:27:01 +0200

    Fix imoprt issues

--- bike_sales_kafka_producer/app/main.py
+++ bike_sales_kafka_producer/app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 from kafka import KafkaProducer
-from get_kafka_producer import producer
+from pydantic import BaseModel
 import json
+import logging
+import os
 
 app = FastAPI()
 
-class SaleData(BaseModel):
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class SalesData(BaseModel):
     Date: str
     Day: int
     Month: int
@@ -26,7 +30,43 @@ class SaleData(BaseModel):
     Cost: float
     Revenue: float
 
+class KafkaProducerSingleton:
+    _instance = None
+
+    @staticmethod
+    def get_instance():
+        """Static access method."""
+        if KafkaProducerSingleton._instance is None:
+            KafkaProducerSingleton()
+        return KafkaProducerSingleton._instance
+
+    def __init__(self):
+        """Virtually private constructor."""
+        if KafkaProducerSingleton._instance is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            # Get the Kafka broker URL from environment variables or default to 'localhost:9092'
+            kafka_broker = os.getenv("KAFKA_BROKER", "kafka1:19092")
+            KafkaProducerSingleton._instance = KafkaProducer(
+                bootstrap_servers=[kafka_broker],
+                value_serializer=lambda v: json.dumps(v).encode('utf-8')
+            )
+
+
+# API endpoint to send data to Kafka
 @app.post("/produce/")
-async def produce(sale: SaleData):
-    producer.send("real_time_data", value=sale.dict())
-    return {"status": "Message sent"}
+async def produce(data: SalesData):
+    try:
+        # Get the Kafka producer instance
+        producer = KafkaProducerSingleton.get_instance()
+
+        # Send data to the Kafka topic "real_time_data"
+        producer.send("real_time_data", value=data.dict())
+
+        # Log the sent data
+        logger.info(f"Message sent: {data.dict()}")
+
+        return {"status": "Message sent"}
+    except Exception as e:
+        logger.error(f"Error sending message: {str(e)}")
+        return {"status": "Failed", "error": str(e)}
