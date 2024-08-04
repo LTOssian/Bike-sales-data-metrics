fd2556ef8dfbf6e3a138926f98d4a55bf5620e24
Louisan Tchitoula <t.louisan@outlook.fr>
2024-08-04 13:28:29 +0200

    Fix sql models and remove enums

--- bike_analytics_db_init/init.sql
+++ bike_analytics_db_init/init.sql
@@ -1,38 +1,15 @@
-CREATE TYPE age_group AS ENUM ('Youth (<25)', 'Young Adults (25-34)', 'Adults (35-64)', 'Seniors (65+)');
-CREATE TYPE customer_gender AS ENUM ('M', 'F');
-CREATE TYPE product_category AS ENUM ('Accessories', 'Bikes', 'Clothing');
-CREATE TYPE sub_category AS ENUM (
-    'Tires and Tubes',
-    'Bottles and Cages',
-    'Road Bikes',
-    'Helmets',
-    'Mountain Bikes',
-    'Jerseys',
-    'Caps',
-    'Fenders',
-    'Touring Bikes',
-    'Gloves',
-    'Cleaners',
-    'Shorts',
-    'Hydration Packs',
-    'Socks',
-    'Vests',
-    'Bike Racks',
-    'Bike Stands'
-);
-
 CREATE TABLE sales_data (
     Date DATE,
     Day INT,
     Month INT,
     Year INT,
     Customer_Age INT,
-    Age_Group age_group,
-    Customer_Gender customer_gender,
+    Age_Group VARCHAR(255),
+    Customer_Gender VARCHAR(255),
     Country VARCHAR(50),
     State VARCHAR(50),
-    Product_Category product_category,
-    Sub_Category sub_category,
+    Product_Category VARCHAR(255),
+    Sub_Category VARCHAR(255),
     Product VARCHAR(100),
     Order_Quantity INT,
     Unit_Cost DECIMAL(10, 2),
